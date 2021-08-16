86169b5abfa9e39ce69ddf6e94124b9e4972173c
Yuta Yotsumoto <y.2000529@gmail.com>
2021-08-16 09:31:00 +0900

    編集

---
+++ hamburgerMenu_branch/hamburgerMenuBranch.js
@@ -0,0 +1,9 @@
+//openBtn
+document.getElementById("hamburgerBtn").onclick = function () {
+  console.log("アイス");
+};
+
+//closeBtn
+document.getElementById("hamburgerBtn").onclick = function () {
+  console.log("アイス");
+};
